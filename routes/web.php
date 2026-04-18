da19f711be328b9d3efc24ab42898588c77afd4e
Bill <bill80362@gmail.com>
2026-04-19 07:26:46 +0800

    feat: 修改首頁路由，將其重定向至管理員登入頁面

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin/login');
 });
 
 Route::get('/invoices/{invoice}/print', [InvoiceController::class, 'print'])
